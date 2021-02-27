396d2e4bc2fd671e4a5c16632cae0cbf2f1e6b70
Joshua <auhsojsomar@gmail.com>
2021-02-27 22:31:39 +0800

    [UPDATED] Sign up page :zap: :recycle:

--- src/App.js
+++ src/App.js
@@ -2,29 +2,31 @@ import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import { Container } from "@material-ui/core";
 import LandingPage from "views/LandingPage";
 import NavHeader from "components/NavHeader";
-import SignInPage from "views/SignInPage";
+import SignInPage from "views/SignIn/SignInPage";
 import SignupPage from "views/SignupPage";
 
 function App() {
   return (
     <Router>
       <Switch>
-        <div className="App">
-          <NavHeader />
-          <Container>
-            <Route exact path="/">
-              <LandingPage />
-            </Route>
+        <>
+          <div className="App">
+            <NavHeader />
+            <Container>
+              <Route exact path="/">
+                <LandingPage />
+              </Route>
 
-            <Route exact path="/signin">
-              <SignInPage />
-            </Route>
+              <Route exact path="/signin">
+                <SignInPage />
+              </Route>
 
-            <Route exact path="/signup">
-              <SignupPage />
-            </Route>
-          </Container>
-        </div>
+              <Route exact path="/signup">
+                <SignupPage />
+              </Route>
+            </Container>
+          </div>
+        </>
       </Switch>
     </Router>
   );
--- src/views/SignInPage.jsx
+++
@@ -1,9 +0,0 @@
-const SignInPage = () => {
-  return (
-    <div>
-      <h1>Sign in Page</h1>
-    </div>
-  );
-};
-
-export default SignInPage;

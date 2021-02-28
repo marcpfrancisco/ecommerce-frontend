535397477b9e79a7874e42577903f959ac632bc9
Marc Francisco <marcpfrancisco@gmail.com>
2021-02-28 13:38:13 +0800

    [FIXED] Styling :lipstick:

--- src/views/SignIn/SignInPage.jsx
+++ src/views/SignIn/SignInPage.jsx
@@ -37,7 +37,7 @@ const TabPanel = (props) => {
       aria-labelledby={`simple-tab-${index}`}
       {...other}
     >
-      {value === index && <div>{children}</div>}
+      {value === index && <>{children}</>}
     </div>
   );
 };
